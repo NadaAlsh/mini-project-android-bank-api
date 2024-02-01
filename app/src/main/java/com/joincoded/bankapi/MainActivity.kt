51bcea0c78784c6a66b0e6480a312bdaaac0e232
83315 <alshaibaninada@gmail.com>
2024-02-01 12:31:03 +0300

    working on ui

--- app/src/main/java/com/joincoded/bankapi/MainActivity.kt
+++ app/src/main/java/com/joincoded/bankapi/MainActivity.kt
@@ -10,7 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.joincoded.bankapi.composable.MyBankApp
 import com.joincoded.bankapi.ui.theme.BankAPITheme
 
--- app/src/main/java/com/joincoded/bankapi/composable/MyBankAPI.kt
+++ app/src/main/java/com/joincoded/bankapi/composable/MyBankAPI.kt
@@ -1,6 +1,7 @@
 package com.joincoded.bankapi.composable
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.background
@@ -33,17 +34,20 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.joincoded.bankapi.data.Card
 import com.joincoded.bankapi.ui.theme.Purple40
 import com.joincoded.bankapi.ui.theme.Purple80
-import androidx.compose.ui.graphics.Color
 import com.joincoded.bankapi.R
 import com.joincoded.bankapi.data.Finance
 import com.joincoded.bankapi.data.Offer
 import com.joincoded.bankapi.data.financeList
+import com.joincoded.bankapi.ui.theme.Green
+import com.joincoded.bankapi.ui.theme.Green2
+import com.joincoded.bankapi.ui.theme.Pink40
+import com.joincoded.bankapi.ui.theme.Pink80
 import com.joincoded.bankapi.ui.theme.PurpleGrey40
-import androidx.navigation.compose.rememberNavController
 import com.joincoded.bankapi.viewmodel.BankViewModel
 
 @Preview
@@ -68,6 +72,7 @@ fun MyBankApp() {
         }
     )
 }
+
 @Preview
 @Composable
 fun TopBar() {
@@ -107,16 +112,18 @@ val cards = listOf(
         cardNumber = "3664 7865 3786 3976",
         cardName = "Business",
         balance = 123.4,
-        color = getGradient(PurpleGrey40, Purple80),
+        color = getGradient(Pink40, Pink80),
         details = "Cardholder: John Doe\nCard Number: ** ** ** 1234\nExpiry Date: 12/24"
-    ),Card(
+    ), Card(
         cardType = "VISA",
         cardNumber = "3664 7865 3786 3976",
         cardName = "Business",
         balance = 123.4,
-        color = getGradient(PurpleGrey40, Purple80),
+        color = getGradient(Green, Green2),
         details = "Cardholder: John Doe\nCard Number: ** ** ** 1234\nExpiry Date: 12/24"
-    ))
+    )
+)
+
 fun getGradient(
     startColor: Color,
     endColor: Color,
@@ -125,6 +132,7 @@ fun getGradient(
         colors = listOf(startColor, endColor)
     )
 }
+
 @Preview
 @Composable
 fun CardsSection() {
@@ -133,11 +141,11 @@ fun CardsSection() {
             .fillMaxWidth()
             .padding(vertical = 28.dp)
     ) {
-        items(cards){ item ->
+        items(cards) { item ->
             CardItem(cardType = item.cardName, cardDetails = item.details, color = item.color)
             Spacer(modifier = Modifier.height(16.dp))
         }
-        }
+    }
 }
 
 @Composable
@@ -164,12 +172,14 @@ fun CardItem(cardType: String, cardDetails: String, color: Brush) {
 @Composable
 fun FinanceSection() {
     Column {
-        Text(text = "Finance",
+        Text(
+            text = "Finance",
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp)
-        )}
+        )
+    }
     val financeList = listOf(
         Finance(
             icon = Icons.Rounded.ExitToApp,
@@ -190,10 +200,10 @@ fun FinanceSection() {
     LazyRow(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 16.dp)
+            .padding(vertical = 1.dp)
     ) {
         items(financeList) {
-            FinanceItem("Deposit",it)
+            FinanceItem("Deposit", it)
             Spacer(modifier = Modifier.height(16.dp))
             FinanceItem("Withdraw", it)
             Spacer(modifier = Modifier.height(16.dp))
@@ -207,15 +217,8 @@ fun FinanceItem(type: String, finance: Finance) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(5.dp)
     ) {
-        Text(
-            text = type,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(text = type, fontWeight = FontWeight.Bold, fontSize = 18.sp, modifier = Modifier.padding(16.dp))
 
         Box(modifier = Modifier.padding(16.dp)) {
             Column(
@@ -227,18 +230,20 @@ fun FinanceItem(type: String, finance: Finance) {
                     .padding(13.dp),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-                Box(modifier = Modifier
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(finance.background)
-                    .padding(6.dp)
-                ){
+                Box(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp))
+                        .background(finance.background)
+                        .padding(6.dp)
+                ) {
                     Icon(
                         imageVector = finance.icon,
                         contentDescription = finance.name,
                         tint = Color.White
                     )
                 }
-                Text(text = finance.name,
+                Text(
+                    text = finance.name,
                     color = MaterialTheme.colorScheme.onSecondaryContainer,
                     fontWeight = FontWeight.SemiBold,
                     fontSize = 15.sp
@@ -248,18 +253,23 @@ fun FinanceItem(type: String, finance: Finance) {
         }
     }
 
-    }
+}
 
-@Preview
+//@Preview
 @Composable
 fun OffersSection() {//+image,size
     Column {
-        Text(text = "Offers",
+        Text(
+            text = "Offers",
             fontSize = 24.sp,
             color = MaterialTheme.colorScheme.onBackground,
             fontWeight = FontWeight.Bold,
             modifier = Modifier.padding(16.dp)
-        )}
+        )
+
+    }
+
+
     val offersList = listOf(
         Offer(
             icon = R.drawable.pick,
@@ -272,7 +282,7 @@ fun OffersSection() {//+image,size
             background = Color.Green
         ),
         Offer(
-            icon = R.drawable.kuwaitairways ,
+            icon = R.drawable.kuwaitairways,
             name = "Kuwait Airways",
             background = Color.Yellow
         )
@@ -282,34 +292,73 @@ fun OffersSection() {//+image,size
             .fillMaxWidth()
             .padding(vertical = 16.dp)
     ) {
-        items(offersList.size) {
-            OfferItem("Pick")
+        items(offersList) {
+            OfferItem("Pick", it)
             Spacer(modifier = Modifier.height(16.dp))
-            OfferItem("Spark Gym")
+            OfferItem("Spark Gym", it)
             Spacer(modifier = Modifier.height(16.dp))
-            OfferItem("Kuwait Airways")
+            OfferItem("Kuwait Airways", it)
 
         }
     }
 }
 
 @Composable
-fun OfferItem(offerName: String) {
+fun OfferItem(offerName: String, offers: Offer) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(5.dp)
     ) {
-        Text(
-            text = offerName,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(16.dp)
-        )
+       // Text(
+       //     text = offerName,
+        //    fontWeight = FontWeight.Bold,
+         //   fontSize = 18.sp,
+         //   modifier = Modifier.padding(16.dp)
+       // )
+        Box(modifier = Modifier.padding(16.dp)) {
+            Column(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(25.dp))
+                    //.background(MaterialTheme.colorScheme.secondaryContainer)
+                    .size(120.dp)
+                    .clickable { }
+                    .padding(13.dp),
+                verticalArrangement = Arrangement.SpaceBetween
+            )
+            {
+                Image(
+                    painter = painterResource(id = offers.icon),
+                    contentDescription = offers.name,
+
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+                Box(
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp))
+                        //.background(offers.background)
+                        .padding(6.dp)
+                ) {
+//                    Image(
+//                        painter = painterResource(id = offers.icon),
+//                        contentDescription = offers.name,
+//
+//                        modifier = Modifier.size(64.dp)
+//                    )
+//                }
+                Spacer(modifier = Modifier.height(8.dp))
+               // Text(
+                 //   text = offers.name,
+                    //color = MaterialTheme.colorScheme.onSecondaryContainer,
+                //    fontWeight = FontWeight.SemiBold,
+                //    fontSize = 15.sp
+              //  )
+            }
+        }
     }
 }
 
-@Preview
 @Composable
 fun TransactionsSection() {
     Column(
